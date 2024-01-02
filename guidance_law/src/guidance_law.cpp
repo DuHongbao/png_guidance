1e37ed4588df2d79361690d995c267dc8f604d96
DuHongbao <derek_dhb@163.com>
2024-01-02 15:00:01 +0800

    finished the guidePNTraj in guidance_law.cpp

--- guidance_law/src/guidance_law.cpp
+++ guidance_law/src/guidance_law.cpp
@@ -140,7 +140,71 @@ bool  GuidanceLaw::calcPNGuideTraj(vehicle &D, vehicle &T, vector<Eigen::Vector3
 }
 
 
-PolynomialTraj GuidanceLaw::guidePNTraj(const Eigen::Vector3d &start_pt, const Eigen::Vector3d &start_vel, const Eigen::Vector3d &start_acc,
+// PolynomialTraj GuidanceLaw::guidePNTraj(const Eigen::Vector3d &start_pt, const Eigen::Vector3d &start_vel, const Eigen::Vector3d &start_acc,
+//                                                     const Eigen::Vector3d &end_pt, const Eigen::Vector3d &end_vel, const Eigen::Vector3d &end_acc, double trajtime){
+//         States state_A(start_pt, start_vel, start_acc);
+//         States state_B(end_pt, end_vel, end_acc);
+//         vehicle  A("drone", state_A), B("target",state_B);
+//         Eigen::Vector3d A_temp_;
+//         vector<Eigen::Vector3d>  Eigentraj;
+//         PolynomialTraj traj;
+//         double t = 0.0, tf = trajtime, ts = 0.5;
+
+//         ////////////calcPNGuideTraj(A,B,dtraj,ttraj);
+//         double distance;
+//         do{
+//                 distance = (A.states().pos - B.states().pos).norm();
+//                 while(t<tf){
+//                         t = 0.0;
+//                         B.update(t);
+//                         A.update(B.states().pos, B.states().vel);
+//                         distance = (A.states().pos - B.states().pos).norm();
+//                         A.getPos(A_temp_);
+//                         Eigentraj.push_back(A_temp_);
+//                         if(distance< 0.5){
+//                                 break;
+//                         }
+//                         t += ts;
+//                 }
+//                 A.setState(state_A);
+//                 B.setState(state_B);
+                
+//                 ts *= 0.5;
+//                 tf *= 1.2;
+//         }while(distance>0.5);
+
+//         ////////////simplify
+
+//         vector<Eigen::Vector3d> newTraj;
+//         int size = Eigentraj.size();
+//         for(int i = 0; i<size; ){
+//                 newTraj.push_back(Eigentraj[i]);
+//                 i +=( size/7 );
+//         }
+//         Eigentraj.clear();
+//         Eigentraj = newTraj;
+
+//         ////////////////// to polytraj
+
+//         Eigen::MatrixXd pos(3, Eigentraj.size());
+//         Eigen::VectorXd time(Eigentraj.size() - 1);
+
+//         for (int i = 0; i < Eigentraj.size(); i++)
+//                 {pos.col(i) = Eigentraj[i];}
+                        
+//         for (int i = 0; i < Eigentraj.size() - 1; i++)
+//                 {time(i) = (pos.col(i + 1) - pos.col(i)).norm() / 1.0;}  //1.0is the max velocity
+
+//         time(0) *= 2.0;
+//         time(time.rows() - 1) *= 2.0;
+
+//         traj = PolynomialTraj::minSnapTraj(pos, start_vel, end_vel, start_acc, end_acc, time);
+
+//         return traj;
+// }
+
+
+PolynomialTraj guidePNTraj(const Eigen::Vector3d &start_pt, const Eigen::Vector3d &start_vel, const Eigen::Vector3d &start_acc,
                                                     const Eigen::Vector3d &end_pt, const Eigen::Vector3d &end_vel, const Eigen::Vector3d &end_acc, double trajtime){
         States state_A(start_pt, start_vel, start_acc);
         States state_B(end_pt, end_vel, end_acc);
@@ -148,7 +212,7 @@ PolynomialTraj GuidanceLaw::guidePNTraj(const Eigen::Vector3d &start_pt, const E
         Eigen::Vector3d A_temp_;
         vector<Eigen::Vector3d>  Eigentraj;
         PolynomialTraj traj;
-        double t = 0.0, tf = trajtime, ts = 0.5;
+        double t = 0.0, tf = trajtime, ts = 1.5;
 
         ////////////calcPNGuideTraj(A,B,dtraj,ttraj);
         double distance;
@@ -162,6 +226,7 @@ PolynomialTraj GuidanceLaw::guidePNTraj(const Eigen::Vector3d &start_pt, const E
                         A.getPos(A_temp_);
                         Eigentraj.push_back(A_temp_);
                         if(distance< 0.5){
+                                cout<<"Find!"<<endl;
                                 break;
                         }
                         t += ts;
@@ -171,19 +236,24 @@ PolynomialTraj GuidanceLaw::guidePNTraj(const Eigen::Vector3d &start_pt, const E
                 
                 ts *= 0.5;
                 tf *= 1.2;
+                cout<<"Find!!!"<<endl;
         }while(distance>0.5);
 
         ////////////simplify
+
 
         vector<Eigen::Vector3d> newTraj;
         int size = Eigentraj.size();
         for(int i = 0; i<size; ){
                 newTraj.push_back(Eigentraj[i]);
-                i +=( size/7 );
+                i +=( size/8 );
         }
         Eigentraj.clear();
         Eigentraj = newTraj;
 
+        for(int i = 0; i< Eigentraj.size();i++){
+                cout<<Eigentraj[i]<<endl;
+        }
         ////////////////// to polytraj
 
         Eigen::MatrixXd pos(3, Eigentraj.size());
@@ -193,13 +263,19 @@ PolynomialTraj GuidanceLaw::guidePNTraj(const Eigen::Vector3d &start_pt, const E
                 {pos.col(i) = Eigentraj[i];}
                         
         for (int i = 0; i < Eigentraj.size() - 1; i++)
-                {time(i) = (pos.col(i + 1) - pos.col(i)).norm() / 1.0;}  //1.0is the max velocity
-
-        time(0) *= 2.0;
-        time(time.rows() - 1) *= 2.0;
-
-        traj = PolynomialTraj::minSnapTraj(pos, start_vel, end_vel, start_acc, end_acc, time);
-
+                {
+                        time(i) = (pos.col(i + 1) - pos.col(i)).norm() / 10.0;
+                        trajTime += time(i);
+                        cout<<"Time:"<<time(i)<<endl;
+                }  
+        //time(0) *= 2.0;
+        //time(time.rows() - 1) *= 2.0;
+        cout<<"Here"<<pos<<endl;
+        Eigen::Vector3d ev = Eigen::Vector3d::Zero();
+        Eigen::Vector3d ea = Eigen::Vector3d::Zero();
+        traj = PolynomialTraj::minSnapTraj(pos, start_vel, ev, start_acc, ea, time);
+       // traj = PolynomialTraj::one_segment_traj_gen(Eigentraj[0], start_vel, start_acc, Eigentraj.back(), ev, ea, 19);
+        
         return traj;
 }
 
