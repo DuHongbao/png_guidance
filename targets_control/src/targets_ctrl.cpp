32d703b8dd8ba04c5848bd165f2de29516196578
DuHongbao <derek_dhb@163.com>
2024-01-06 05:32:20 +0800

    delete bspline_pub.cpp in targets_control and change targets_ctrl.cpp

--- targets_control/src/targets_ctrl.cpp
+++ targets_control/src/targets_ctrl.cpp
@@ -26,10 +26,10 @@ int traj_id_;
 double last_yaw_, last_yaw_dot_;
 double time_forward_;
 
-int  ctrlpts_size = 50;
+int  ctrlpts_size = 204;
 
-  Eigen::Vector3d startPoint(10.0, 0.0, 1.0);
-  Eigen::Vector3d  moveVector(0.1, -0.4, 0.0);
+Eigen::Vector3d startPoint(10.0, 0.0, 1.0);
+Eigen::Vector3d  moveVector(0.1, 5.0, 0.0);
 
 void bsplineCallback(drone_trajs::BsplineConstPtr msg)
 {
@@ -78,57 +78,107 @@ void bsplineCallback(drone_trajs::BsplineConstPtr msg)
   receive_traj_ = true;
 }
 
-
-void bspline()
+void bspline_()
 {
-  // parse pos traj
+        // parse pos traj
 
-  Eigen::MatrixXd pos_pts(3, ctrlpts_size-4);
+        Eigen::MatrixXd pos_pts(3, ctrlpts_size-4);
 
-  Eigen::VectorXd knots(ctrlpts_size);
-  for (int i = 0; i < ctrlpts_size; ++i)
-  {
-    knots(i) = -0.9 + i*0.3;
-    if(knots(i)>-0.01&&knots(i)<0.01){
-        knots(i) = 0.0;
-    }
-    cout<<"knots_"<<i<<":"<<knots(i)<<endl;
-  }
+        Eigen::VectorXd knots(ctrlpts_size);
+        for (int i = 0; i < ctrlpts_size; ++i)
+        {
+                knots(i) = -0.9 + i*0.3;
+                if(knots(i)>-0.01&&knots(i)<0.01){
+                        knots(i) = 0.0;
+                }
+        }
 
-  for (int i = 0; i < ctrlpts_size - 4; ++i)
-  {
-    pos_pts(0, i) = startPoint[0] + moveVector[0]*i*sin(0.5*i);
-    pos_pts(1, i) = startPoint[1] + moveVector[1]*i;
-    pos_pts(2, i) = startPoint[2] + moveVector[2]*i;
-  }
+        for (int i = 0; i < ctrlpts_size - 4; ++i)
+        {
+                pos_pts(0, i) = startPoint[0] + 4.0*moveVector[0]*cos(0.031415926*i);
+                pos_pts(1, i) = startPoint[1] + 4.0*moveVector[1]* sin(0.031415926*i);
+                pos_pts(2, i) = startPoint[2] + moveVector[2]*i;
+        }
 
 
-  UniformBspline pos_traj(pos_pts, 3, 0.1);
-  pos_traj.setKnot(knots);
+        UniformBspline pos_traj(pos_pts, 3, 0.1);
+        pos_traj.setKnot(knots);
+                
+        // parse yaw traj
+
+        // Eigen::MatrixXd yaw_pts(msg->yaw_pts.size(), 1);
+        // for (int i = 0; i < msg->yaw_pts.size(); ++i) {
+        //   yaw_pts(i, 0) = msg->yaw_pts[i];
+        // }
+
+        //UniformBspline yaw_traj(yaw_pts, msg->order, msg->yaw_dt);
+
+        start_time_ = ros::Time::now();
+        traj_id_ = 1 ;
+                
+        traj_.clear();
+        traj_.push_back(pos_traj);
+        traj_.push_back(traj_[0].getDerivative());
+        traj_.push_back(traj_[1].getDerivative());
+
+                
+        traj_duration_ = traj_[0].getTimeSum();
+
+        cout<<"traj_duration_:"<<traj_duration_<<endl;
+
+        receive_traj_ = true;
         
-  // parse yaw traj
+}
 
-  // Eigen::MatrixXd yaw_pts(msg->yaw_pts.size(), 1);
-  // for (int i = 0; i < msg->yaw_pts.size(); ++i) {
-  //   yaw_pts(i, 0) = msg->yaw_pts[i];
-  // }
+void bspline(const ros::TimerEvent &e)
+{
+        // parse pos traj
 
-  //UniformBspline yaw_traj(yaw_pts, msg->order, msg->yaw_dt);
+        Eigen::MatrixXd pos_pts(3, ctrlpts_size-4);
 
-  start_time_ = ros::Time::now();
-  traj_id_ = 1 ;
-        
-  traj_.clear();
-  traj_.push_back(pos_traj);
-  ROS_WARN("HERE");
-  traj_.push_back(traj_[0].getDerivative());
- ROS_WARN("HERE!"); 
-  traj_.push_back(traj_[1].getDerivative());
+        Eigen::VectorXd knots(ctrlpts_size);
+        for (int i = 0; i < ctrlpts_size; ++i)
+        {
+                knots(i) = -0.9 + i*0.3;
+                if(knots(i)>-0.01&&knots(i)<0.01){
+                        knots(i) = 0.0;
+                }
+        }
 
-        
-  traj_duration_ = traj_[0].getTimeSum();
+        for (int i = 0; i < ctrlpts_size - 4; ++i)
+        {
+                pos_pts(0, i) = startPoint[0] + 4.0*moveVector[0]*cos(0.031415926*i);
+                pos_pts(1, i) = startPoint[1] + 4.0*moveVector[1]* sin(0.031415926*i);
+                pos_pts(2, i) = startPoint[2] + moveVector[2]*i;
+        }
 
-  receive_traj_ = true;
+
+        UniformBspline pos_traj(pos_pts, 3, 0.1);
+        pos_traj.setKnot(knots);
+                
+        // parse yaw traj
+
+        // Eigen::MatrixXd yaw_pts(msg->yaw_pts.size(), 1);
+        // for (int i = 0; i < msg->yaw_pts.size(); ++i) {
+        //   yaw_pts(i, 0) = msg->yaw_pts[i];
+        // }
+
+        //UniformBspline yaw_traj(yaw_pts, msg->order, msg->yaw_dt);
+
+        start_time_ = ros::Time::now();
+        traj_id_ = 1 ;
+                
+        traj_.clear();
+        traj_.push_back(pos_traj);
+        traj_.push_back(traj_[0].getDerivative());
+        traj_.push_back(traj_[1].getDerivative());
+
+                
+        traj_duration_ = traj_[0].getTimeSum();
+
+        cout<<"traj_duration_:"<<traj_duration_<<endl;
+
+        receive_traj_ = true;
   
 }
 
@@ -227,105 +277,116 @@ std::pair<double, double> calculate_yaw(double t_cur, Eigen::Vector3d &pos, ros:
 void cmdCallback(const ros::TimerEvent &e)
 {
 
-  /* no publishing before receive traj_ */
-  if (!receive_traj_)
-    return;
-  
-  ros::Time time_now = ros::Time::now();
-  double t_cur = (time_now - start_time_).toSec();
+        /* no publishing before receive traj_ */
+        if (!receive_traj_)
+        return;
+        
+        ros::Time time_now = ros::Time::now();
+        double t_cur = (time_now - start_time_).toSec();
 
-  Eigen::Vector3d pos(Eigen::Vector3d::Zero()), vel(Eigen::Vector3d::Zero()), acc(Eigen::Vector3d::Zero()), pos_f;
-  std::pair<double, double> yaw_yawdot(0, 0);
+        Eigen::Vector3d pos(Eigen::Vector3d::Zero()), vel(Eigen::Vector3d::Zero()), acc(Eigen::Vector3d::Zero()), pos_f;
+        std::pair<double, double> yaw_yawdot(0, 0);
 
-  static ros::Time time_last = ros::Time::now();
-  if (t_cur < traj_duration_ && t_cur >= 0.0)
-  {
-    pos = traj_[0].evaluateDeBoorT(t_cur);
-    vel = traj_[1].evaluateDeBoorT(t_cur);
-    acc = traj_[2].evaluateDeBoorT(t_cur);
+        static ros::Time time_last = ros::Time::now();
+        if (t_cur < traj_duration_ && t_cur >= 0.0)
+        {
+                pos = traj_[0].evaluateDeBoorT(t_cur);
+                vel = traj_[1].evaluateDeBoorT(t_cur);
+                acc = traj_[2].evaluateDeBoorT(t_cur);
 
-    /*** calculate yaw ***/
-    yaw_yawdot = calculate_yaw(t_cur, pos, time_now, time_last);
-    /*** calculate yaw ***/
+                /*** calculate yaw ***/
+                yaw_yawdot = calculate_yaw(t_cur, pos, time_now, time_last);
+                /*** calculate yaw ***/
 
-    double tf = min(traj_duration_, t_cur + 2.0);
-    pos_f = traj_[0].evaluateDeBoorT(tf);
-  }
-  else if (t_cur >= traj_duration_)
-  {
-    /* hover when finish traj_ */
-    pos = traj_[0].evaluateDeBoorT(traj_duration_);
-    vel.setZero();
-    acc.setZero();
+                double tf = min(traj_duration_, t_cur + 2.0);
+                pos_f = traj_[0].evaluateDeBoorT(tf);
+        }
+        else if (t_cur >= traj_duration_)
+        {
+                /* hover when finish traj_ */
+                pos = traj_[0].evaluateDeBoorT(traj_duration_);
+                vel.setZero();
+                acc.setZero();
 
-    yaw_yawdot.first = last_yaw_;
-    yaw_yawdot.second = 0;
+                yaw_yawdot.first = last_yaw_;
+                yaw_yawdot.second = 0;
 
-    pos_f = pos;
-    return;
-  }
-  else
-  {
-    cout << "[Traj server]: invalid time." << endl;
-  }
-  time_last = time_now;
+                pos_f = pos;
+                return;
+        }
+        else
+        {
+                cout << "[Traj server]: invalid time." << endl;
+        }
+        time_last = time_now;
 
-  cmd.header.stamp = time_now;
-  cmd.header.frame_id = "world";
-  cmd.trajectory_flag = quadrotor_msgs::PositionCommand::TRAJECTORY_STATUS_READY;
-  cmd.trajectory_id = traj_id_;
+        cmd.header.stamp = time_now;
+        cmd.header.frame_id = "world";
+        cmd.trajectory_flag = quadrotor_msgs::PositionCommand::TRAJECTORY_STATUS_READY;
+        cmd.trajectory_id = traj_id_;
 
-  cmd.position.x = pos(0);
-  cmd.position.y = pos(1);
-  cmd.position.z = pos(2);
+        cmd.position.x = pos(0);
+        cmd.position.y = pos(1);
+        cmd.position.z = pos(2);
 
-  cmd.velocity.x = vel(0);
-  cmd.velocity.y = vel(1);
-  cmd.velocity.z = vel(2);
+        cmd.velocity.x = vel(0);
+        cmd.velocity.y = vel(1);
+        cmd.velocity.z = vel(2);
 
-  cmd.acceleration.x = acc(0);
-  cmd.acceleration.y = acc(1);
-  cmd.acceleration.z = acc(2);
+        cmd.acceleration.x = acc(0);
+        cmd.acceleration.y = acc(1);
+        cmd.acceleration.z = acc(2);
 
-  cmd.yaw = yaw_yawdot.first;
-  cmd.yaw_dot = yaw_yawdot.second;
+        cmd.yaw = yaw_yawdot.first;
+        cmd.yaw_dot = yaw_yawdot.second;
 
-  last_yaw_ = cmd.yaw;
+        last_yaw_ = cmd.yaw;
 
-  pos_cmd_pub.publish(cmd);
+        pos_cmd_pub.publish(cmd);
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "targets_ctrl");
-  // ros::NodeHandle node;
-  ros::NodeHandle nh("~");
+        ros::init(argc, argv, "targets_ctrl");
+        // ros::NodeHandle node;
+        ros::NodeHandle nh("~");
+        double loop_rate_hz = 1.0 / 59.1; // 将周期转换为频率，即每秒循环的次数  
+        ros::Rate loop_rate(loop_rate_hz);
 
-  //ros::Subscriber bspline_sub = nh.subscribe("/drone_0_planning/bspline", 10, bsplineCallback);
-  bspline();
 
-  pos_cmd_pub = nh.advertise<quadrotor_msgs::PositionCommand>("/target2_cmd", 50);
+        bspline_();
+        
 
-  ros::Timer cmd_timer = nh.createTimer(ros::Duration(0.01), cmdCallback);
 
-  /* control parameter */
-  cmd.kx[0] = pos_gain[0];
-  cmd.kx[1] = pos_gain[1];
-  cmd.kx[2] = pos_gain[2];
+        //ros::Subscriber bspline_sub = nh.subscribe("/drone_0_planning/bspline", 10, bsplineCallback);
 
-  cmd.kv[0] = vel_gain[0];
-  cmd.kv[1] = vel_gain[1];
-  cmd.kv[2] = vel_gain[2];
 
-  time_forward_ = 1.0;
-  last_yaw_ = 0.0;
-  last_yaw_dot_ = 0.0;
+        ros::Timer bspline_timer = nh.createTimer(ros::Duration(59.1), bspline);
 
-  ros::Duration(1.0).sleep();
+        pos_cmd_pub = nh.advertise<quadrotor_msgs::PositionCommand>("/target2_cmd", 50);
 
-  ROS_WARN("[Traj server]: ready.");
+        ros::Timer cmd_timer = nh.createTimer(ros::Duration(0.01), cmdCallback);
 
-  ros::spin();
+        /* control parameter */
+        cmd.kx[0] = pos_gain[0];
+        cmd.kx[1] = pos_gain[1];
+        cmd.kx[2] = pos_gain[2];
 
-  return 0;
+        cmd.kv[0] = vel_gain[0];
+        cmd.kv[1] = vel_gain[1];
+        cmd.kv[2] = vel_gain[2];
+
+        time_forward_ = 1.0;
+        last_yaw_ = 0.0;
+        last_yaw_dot_ = 0.0;
+
+        ros::Duration(1.0).sleep();
+
+        ROS_WARN("[Traj server]: ready.");
+
+
+        ros::spin();
+
+
+        return 0;
 }
